1b2d865352ac1e1b3382ab5bac7ec0bb8fc4f9e2
Nathan Kennedy <nathan.kennedy.1997@gmail.com>
2020-11-16 16:35:49 +1300

    removed unneeded comments

--- index.js
+++ index.js
@@ -2,45 +2,21 @@
 /* eslint-disable no-tabs */
 /* eslint-disable dot-notation */
 /* eslint-disable quote-props */
-// const { Validator } = require('jsonschema');
 const { promises: fsProm } = require('fs');
 
-const configGen = require('./generators/buildConfig');
-const detailsGen = require('./generators/buildDetails');
+const configGenerator = require('./generators/buildConfig');
+const detailsGenerator = require('./generators/buildDetails');
 const helpers = require('./helpers/helpers');
-
-/*
-function runValidation(config) {
-	const v = new Validator();
-
-	const results = v.validate(config, schema);
-
-	if (results.errors.length) {
-		console.warn('Config validation errored');
-		console.warn(results.errors);
-	} else {
-		console.log('Config succusfully validated');
-	}
-
-	const resultsDetails = v.validate(detailsTest, detailsSchema);
-	if (resultsDetails.errors.length) {
-		console.warn('Details validation errored');
-		console.warn(resultsDetails.errors);
-	} else {
-		console.log('Details succusfully validated');
-	}
-}
-*/
 
 (async function main() {
 	const args = process.argv;
 
 	const hugoConfig = await helpers.getHugoConfig(args);
 
-	const config = await configGen.generateConfig(hugoConfig);
+	const config = await configGenerator.generateConfig(hugoConfig);
 	const configData = JSON.stringify(config, null, 4);
 
-	const details = await detailsGen.generateDetails(hugoConfig);
+	const details = await detailsGenerator.generateDetails(hugoConfig);
 	const detailsData = JSON.stringify(details, null, 4);
 
 	const { publish } = helpers.getPaths(hugoConfig);
@@ -56,5 +32,4 @@ function runValidation(config) {
 	} catch (writeError) {
 		console.error(`error writing to ${publish}/_cloudcannon/`);
 	}
-	// runValidation(config);
 }());
