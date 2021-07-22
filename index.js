0db72a59ec7b773df9c19a54908b336d72c4fb35
Nathan Kennedy <nathan.kennedy.1997@gmail.com>
2021-07-22 15:07:33 +1200

    Fixed buildinfo not being outputted to source dir

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const fs = require('fs').promises;
+const { join } = require('path');
 const infoGenerator = require('./generators/build-info');
 const hugoHelper = require('./helpers/hugo-config');
 const pathHelper = require('./helpers/paths');
@@ -14,13 +15,14 @@ const pathHelper = require('./helpers/paths');
 	const info = await infoGenerator.generateInfo(hugoConfig);
 	const infoData = JSON.stringify(info, null, 4);
 
-	const { publish } = pathHelper.getPaths();
+	const { source, publish } = pathHelper.getPaths();
+	const outputDir = join(source, publish, '_cloudcannon');
 
 	console.log('writing...');
 	try {
-		await fs.mkdir(`${publish}/_cloudcannon`, { recursive: true });
-		await fs.writeFile(`${publish}/_cloudcannon/info.json`, infoData);
+		await fs.mkdir(`${outputDir}`, { recursive: true });
+		await fs.writeFile(`${outputDir}/info.json`, infoData);
 	} catch (writeError) {
-		console.error(`error writing to ${publish}/_cloudcannon/`);
+		console.error(`error writing to ${outputDir}/`);
 	}
 }());
