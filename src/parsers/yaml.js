2e29a9e69e7337ee02ab51150e43edb93ab7f8cc
Tate <tate@cloudcannon.com>
2026-06-24 10:48:37 +1200

    Use js-yaml schema that matches the previous default behaviour

--- src/parsers/yaml.js
+++ src/parsers/yaml.js
@@ -1,9 +1,9 @@
-import { load } from 'js-yaml';
+import { load, YAML11_SCHEMA } from 'js-yaml';
 import log from '../helpers/logger.js';
 
 export function parseYaml(data) {
 	try {
-		return load(data, { json: true });
+		return load(data, { json: true, schema: YAML11_SCHEMA });
 	} catch (parseError) {
 		log(parseError, 'error');
 	}
